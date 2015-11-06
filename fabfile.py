9c23819cdb38ef6b0fa88dccfb9327e49da42e1c
Warwick Stone <uozu.aho@gmail.com>
2015-11-06 18:14:01 +1100

    add fabric pi management

---
+++ fabfile.py
@@ -0,0 +1,28 @@
+from fabric.api import env
+from fabric.operations import run, put
+
+import config
+
+# get hosts
+hosts = []
+with open(config.IP_LIST_FILE) as infile:
+    hosts.append('pi@' + config.IP_BASE_ADDR + infile.readline().strip())
+env.hosts = hosts
+
+
+def test():
+    run('echo yo!')
+
+
+def pi_setup():
+    # In case the default rpi servers aren't responding, add these lines to /etc/apt/sources.list
+    # deb http://mirror.aarnet.edu.au/pub/raspbian/raspbian wheezy main contrib non-free rpi
+    # deb-src http://mirror.aarnet.edu.au/pub/raspbian/raspbian wheezy main contrib non-free rpi
+    run('sudo apt-get update')
+    run('sudo apt-get install -y python-twisted')
+
+
+def deploy():
+    print 'Make sure config.py is correct!'
+    put('config.py', config.listener.DEPLOY_DIR)
+    put('listen.py', config.listener.DEPLOY_DIR)
--- readme.md
+++ readme.md
@@ -11,6 +11,12 @@ Original project found on [Instructables](http://www.instructables.com/id/Multip
 # Software
 - python scripts all tested using python 2
 
+## Dependencies
+- [fabric](http://www.fabfile.org/) used for some pi task management
+- [twisted](https://twistedmatrix.com/trac/) used for networking
+    + twisted isn't installed by default on the pi. You can install it
+      using fabric: `fab pi_setup` (not the only way, of course...)
+
 # TODO:
 - Speed up taking a picture. Currently takes about 7 seconds - maybe use
   the picamera python module to directly interface with the camera in the listen
